c947115d821a18c90bd715f20918230f38da85e7
adrogatnivek <kevinclark.tagorda@gmail.com>
2023-07-07 10:24:10 +0800

    added alert

--- index.js
+++ index.js
@@ -15,6 +15,15 @@ document.getElementById('form-fields').addEventListener('input', function (event
   }
 });
 
+ function showAlert(message, type) {
+    var alertContainer = document.createElement('div');
+    alertContainer.className = 'alert alert-' + type;
+    alertContainer.textContent = message;
+    document.getElementById('form-fields').prepend(alertContainer);
+  }
+
+
+
 function submitForm() {
   var fullName = document.getElementById('fullName').value;
   var emailAdd = document.getElementById('emailAdd').value;
@@ -29,12 +38,6 @@ function submitForm() {
   sendMail();
 }
 
- function showAlert(message, type) {
-    var alertContainer = document.createElement('div');
-    alertContainer.className = 'alert alert-' + type;
-    alertContainer.textContent = message;
-    document.getElementById('form-fields').prepend(alertContainer);
-  }
 
 
 function sendMail() {
@@ -65,91 +68,3 @@ function sendMail() {
      return false;
 }
 
-
-
-// function sendMail() {
-//   var params = {
-//     fullName: document.getElementById("fullName").value,
-//     contactNumber: document.getElementById("contactNumber").value,
-//     emailAdd: document.getElementById("emailAdd").value,
-//     sendMessage: document.getElementById("sendMessage").value,
-//   };
-
-//   const serviceID = "service_9jiikdt";
-//   const templateID = "template_f9djjsq";
-
-//     emailjs.send(serviceID, templateID, params)
-//     .then(res=>{
-//         document.getElementById("fullName").value = "";
-//         document.getElementById("contactNumber").value = "";
-//         document.getElementById("emailAdd").value = "";
-//         document.getElementById("sendMessage").value = "";
-//         console.log(res);
-//         alert("Your message sent successfully!!")
-
-//     })
-//     .catch(err=>console.log(err));
-
-// }
-// function sendMail() {
-//   var fullName = document.getElementById('fullName').value;
-//   var contactNumber = document.getElementById('contactNumber').value;
-//   var emailAdd = document.getElementById('emailAdd').value;
-//   var sendMessage = document.getElementById('sendMessage').value;
-
-//   if (fullName === '' || contactNumber === '' || emailAdd === '' || sendMessage === '') {
-//     alert('Please fill in all required fields.');
-//     return;
-//   }
-
-//   var params = {
-//     fullName: fullName,
-//     contactNumber: contactNumber,
-//     emailAdd: emailAdd,
-//     sendMessage: sendMessage
-//   };
-
-//   const serviceID = "service_9jiikdt";
-//   const templateID = "template_f9djjsq";
-
-//   emailjs.send(serviceID, templateID, params)
-//     .then(res => {
-//       document.getElementById("fullName").value = "";
-//       document.getElementById("contactNumber").value = "";
-//       document.getElementById("emailAdd").value = "";
-//       document.getElementById("sendMessage").value = "";
-//       console.log(res);
-//       alert("Your message sent successfully!!");
-//     })
-//     .catch(err => console.log(err));
-// }
-
-// document.getElementById('form-fields').addEventListener('submit', function (event) {
-//   event.preventDefault(); // Prevent the form from submitting
-
-//   var fullName = document.getElementById('fullName').value;
-//   var contactNumber = document.getElementById('contactNumber').value;
-//   var emailAdd = document.getElementById('emailAdd').value;
-//   var sendMessage = document.getElementById('sendMessage').value;
-
-//   if (fullName === '' || contactNumber === '' || emailAdd === '' || sendMessage === '') {
-//     alert('Please fill in all required fields.');
-//     return;
-//   }
-
-//   sendMail(); // Call the sendMail function to send the email
-// });
-
-// document.getElementById('form-fields').addEventListener('input', function () {
-//   var fullName = document.getElementById('fullName').value;
-//   var contactNumber = document.getElementById('contactNumber').value;
-//   var emailAdd = document.getElementById('emailAdd').value;
-//   var sendMessage = document.getElementById('sendMessage').value;
-//   var submitButton = document.getElementById('form-btn');
-
-//   if (fullName !== '' && contactNumber !== '' && emailAdd !== '' && sendMessage !== '') {
-//     submitButton.disabled = false;
-//   } else {
-//     submitButton.disabled = true;
-//   }
-// });
